ab918b2664b8d28d0c073ff2b9917286165c39bf
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2021-05-30 22:43:41 +0500

    created Project

---
+++ BlazorApp1/BlazorApp1/Shared/Catalog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Shared
+{
+    public class Catalog
+    {
+
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Shared/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Shared
+{
+    class Product
+    {
+        public Product(int id,string name,string desc,double price)
+        {
+            this.Id = id;
+            this.name = name;
+            this.description = desc;
+            this.price = price;
+        }
+
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+
+    }
+}
